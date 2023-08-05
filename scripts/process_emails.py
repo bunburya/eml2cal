7610e36173c002040366b54a89f3b0d1d4e6f0ce
bunburya <dev@bunburya.eu>
2023-08-05 14:56:37 +0100

    work on scripts

--- scripts/process_emails.py
+++ scripts/process_emails.py
@@ -1,8 +1,11 @@
+import sys
 import os
 import subprocess
+import smtplib
+from datetime import datetime
 from mailbox import Mailbox, Maildir
-from email.message import Message
-from typing import Optional, Collection
+from email.message import Message, EmailMessage
+from typing import Optional, Collection, Iterable
 
 from icalendar import Calendar, Event, vCalAddress, vText
 
@@ -84,22 +87,83 @@ def process_mailbox(
             add_attendee(c, email_addr)
         return c, emails
 
-if __name__ == "__main__":
-    import sys
-    mdir = sys.argv[1]
-    email_addr = sys.argv[2]
+def get_pass(pass_name: str) -> str:
+    """Get password for smtp, using `pass`."""
+    cmd = ["/usr/bin/pass", "show", pass_name]
+    output = subprocess.run(cmd, capture_output=True)
+    return output.stdout.decode().strip()
+
+def email_calendar(
+        to: str,
+        subject: str,
+        event_details: Iterable[str],
+        cal: Calendar,
+        sender: str,
+        passwd: str,
+        smtp_server: str,
+        smtp_port: int = 587,
+    ):
+    """Send an email with attachment."""
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = to
+    msg.set_content('\n'.join(event_details))
+    msg.add_attachment(
+        cal.to_ical(), 
+        maintype="text", 
+        subtype="calendar",
+        filename="events.ics"
+    )
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(sender, passwd)
+        server.send_message(msg)
+
+def main():
+    _, mdir, email_addr, pass_name, smtp_server = sys.argv
+    mail_addr = sys.argv[2]
     if len(sys.argv) > 3:
         CMD.extend(["--additional-search-path", sys.argv[3]])
     result = process_mailbox(Maildir(mdir), email_addr)
     if result is not None:
         cal, emails = result
-        os.makedirs("calendars")
-        with open("output.ics", "wb") as f:
-            f.write(cal.to_ical())
-        with open("emails.txt", "w") as f:
-            for e in emails:
-                f.write(f"{e}\n")
-        print(f"{len(emails)} event emails found.")
-    else:
-        print(f"0 event emails found.")
+        email_calendar(
+            email_addr,
+            f"Events: {datetime.now()}",
+            emails,
+            cal,
+            email_addr,
+            get_pass(pass_name),
+            smtp_server
+            )
+    #    os.makedirs("calendars")
+    #    with open("output.ics", "wb") as f:
+    #        f.write(cal.to_ical())
+    #    with open("emails.txt", "w") as f:
+    #        for e in emails:
+    #            f.write(f"{e}\n")
+    #    print(f"{len(emails)} event emails found.")
+    #else:
+    #    print(f"0 event emails found.")
+
+def test():
+    _, event_detail_file, cal_file, email_addr, pass_name, smtp_server = sys.argv
+    with open(event_detail_file) as f:
+        event_details = f.readlines()
+    with open(cal_file, "rb") as f:
+        cal = Calendar.from_ical(f.read())
+    email_calendar(
+        email_addr,
+        "Test email",
+        event_details,
+        cal,
+        email_addr,
+        get_pass(pass_name),
+        smtp_server
+    )
+
+if __name__ == "__main__":
+    #test()
+    main()
 
